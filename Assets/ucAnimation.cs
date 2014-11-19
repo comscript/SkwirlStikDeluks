c587209e957b8179ae9371e05c851c8f36ea44dd
oeihiko <oeihiko@gmail.com>
2014-11-18 21:01:46 -0500

    Add some walk, run and jump animations

---
+++ Assets/ucAnimation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ucAnimation : MonoBehaviour {
+
+	public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+		anim.SetFloat ("Speed", v);
+		anim.SetFloat ("Direction", h);
+		anim.SetBool("Jump", Input.GetButtonDown ("Jump"));
+	}
+}
